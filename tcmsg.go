100108490191241e247ed726db5ffa1bb9032123
Lehner Florian <dev@der-flo.net>
2019-04-05 10:35:13 +0200

    Add tcmsg struct
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ tcmsg.go
@@ -0,0 +1,28 @@
+package rtnetlink
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+type Tcmsg struct {
+	Family  uint32
+	Ifindex uint32
+	Handle  uint32
+	Parent  uint32
+	Info    uint32
+}
+
+func tcmsgEncode(i Tcmsg) ([]byte, error) {
+	var tcm bytes.Buffer
+	err := binary.Write(&tcm, nativeEndian, i)
+	return tcm.Bytes(), err
+}
+
+func tcmsgDecode(data []byte, tc *Tcmsg) error {
+	b := bytes.NewReader(data)
+	if err := binary.Read(b, nativeEndian, tc); err != nil {
+		return err
+	}
+	return nil
+}
