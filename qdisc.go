804646c795cce7604727279a2f0649ac3978d75c
Lehner Florian <dev@der-flo.net>
2019-04-23 12:27:01 +0200

    Extend qdisc API
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- qdisc.go
+++ qdisc.go
@@ -22,61 +22,54 @@ func (tc *Tc) Qdisc() *Qdisc {
 	return &Qdisc{*tc}
 }
 
-// New adds a queueing discipline
-func (qd *Qdisc) New(info *Object) error {
+// Add creates a new queueing discipline
+func (qd *Qdisc) Add(info *Object) error {
 	options, err := validateQdiscObject(rtmNewQdisc, info)
 	if err != nil {
 		return err
 	}
-	return qd.action(rtmNewQdisc, info, options)
+	return qd.action(rtmNewQdisc, netlink.Create|netlink.Excl, info, options)
 }
 
-// Del removes a queueing discipline
-func (qd *Qdisc) Del(info *Object) error {
+// Replace add/remove a queueing discipline. If the node does not exist yet it is created
+func (qd *Qdisc) Replace(info *Object) error {
+	options, err := validateQdiscObject(rtmNewQdisc, info)
+	if err != nil {
+		return err
+	}
+	return qd.action(rtmNewQdisc, netlink.Create|netlink.Replace, info, options)
+}
+
+// Link performs a replace on an existing queueing discipline
+func (qd *Qdisc) Link(info *Object) error {
+	options, err := validateQdiscObject(rtmNewQdisc, info)
+	if err != nil {
+		return err
+	}
+	return qd.action(rtmNewQdisc, netlink.Replace, info, options)
+}
+
+// Delte removes a queueing discipline
+func (qd *Qdisc) Delte(info *Object) error {
 	options, err := validateQdiscObject(rtmDelQdisc, info)
 	if err != nil {
 		return err
 	}
-	return qd.action(rtmDelQdisc, info, options)
+	return qd.action(rtmDelQdisc, netlink.HeaderFlags(0), info, options)
+}
+
+// Change modifies a queueing discipline 'in place'
+func (qd *Qdisc) Change(info *Object) error {
+	options, err := validateQdiscObject(rtmNewQdisc, info)
+	if err != nil {
+		return err
+	}
+	return qd.action(rtmNewQdisc, netlink.HeaderFlags(0), info, options)
 }
 
 // Get fetches all queueing disciplines
 func (qd *Qdisc) Get() ([]Object, error) {
-	var results []Object
-
-	tcminfo, err := tcmsgEncode(&Msg{})
-	if err != nil {
-		return results, err
-	}
-
-	var data []byte
-	data = append(data, tcminfo...)
-
-	req := netlink.Message{
-		Header: netlink.Header{
-			Type:  netlink.HeaderType(rtmGetQdisc),
-			Flags: netlink.Request | netlink.Dump,
-		},
-		Data: data,
-	}
-
-	msgs, err := qd.query(req)
-	if err != nil {
-		return results, err
-	}
-
-	for _, msg := range msgs {
-		result := Object{}
-		if err := tcmsgDecode(msg.Data[:20], &result.Msg); err != nil {
-			return results, nil
-		}
-		if err := extractTcmsgAttributes(msg.Data[20:], &result.Attribute); err != nil {
-			return results, nil
-		}
-		results = append(results, result)
-	}
-
-	return results, nil
+	return qd.get(rtmGetQdisc, &Msg{})
 }
 
 func validateQdiscObject(action int, info *Object) ([]tcOption, error) {
@@ -85,10 +78,14 @@ func validateQdiscObject(action int, info *Object) ([]tcOption, error) {
 		return options, fmt.Errorf("Could not set device ID 0")
 	}
 
-	if info.Kind != "clsact" {
+	// TODO: improve logic and check combinations
+
+	switch info.Kind {
+	case "clsact":
+		options = append(options, tcOption{Interpretation: vtString, Type: tcaKind, Data: info.Kind})
+	default:
 		return options, ErrNotImplemented
 	}
-	options = append(options, tcOption{Interpretation: vtString, Type: tcaKind, Data: info.Kind})
 
 	if info.Stats != nil || info.XStats != nil || info.Stats2 != nil || info.FqCodel != nil || info.BPF != nil {
 		return options, ErrNotImplemented
