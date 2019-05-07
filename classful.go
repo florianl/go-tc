ef0ecf20f58be8ea6fdc5ca9c921f076d0d9c4c0
Lehner Florian <dev@der-flo.net>
2019-05-07 11:47:39 +0200

    Add more classful algorithms
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- classful.go
+++ classful.go
@@ -1,6 +1,8 @@
 package tc
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/mdlayher/netlink"
@@ -35,4 +37,63 @@ func extractQfqOptions(data []byte, info *Qfq) error {
 		}
 	}
 	return nil
+}
+
+const (
+	tcaHfscUnspec = iota
+	tcaHfscRsc
+	tcaHfscFsc
+	tcaHfscUsc
+)
+
+// Hfsc contains attributes of the hfsc discipline
+type Hfsc struct {
+	Rsc *ServiceCurve
+	Fsc *ServiceCurve
+	Usc *ServiceCurve
+}
+
+func extractHfscOptions(data []byte, info *Hfsc) error {
+	ad, err := netlink.NewAttributeDecoder(data)
+	if err != nil {
+		return err
+	}
+	ad.ByteOrder = nativeEndian
+	for ad.Next() {
+		switch ad.Type() {
+		case tcaHfscRsc:
+			curve := &ServiceCurve{}
+			if err := extractServiceCurve(ad.Bytes(), curve); err != nil {
+				return err
+			}
+			info.Rsc = curve
+		case tcaHfscFsc:
+			curve := &ServiceCurve{}
+			if err := extractServiceCurve(ad.Bytes(), curve); err != nil {
+				return err
+			}
+			info.Fsc = curve
+		case tcaHfscUsc:
+			curve := &ServiceCurve{}
+			if err := extractServiceCurve(ad.Bytes(), curve); err != nil {
+				return err
+			}
+			info.Usc = curve
+		default:
+			return fmt.Errorf("extractHfscOptions()\t%d\n\t%v", ad.Type(), ad.Bytes())
+		}
+	}
+	return nil
+}
+
+// ServiceCurve from include/uapi/linux/pkt_sched.h
+type ServiceCurve struct {
+	M1 uint32
+	D  uint32
+	M2 uint32
+}
+
+func extractServiceCurve(data []byte, info *ServiceCurve) error {
+	b := bytes.NewReader(data)
+	return binary.Read(b, nativeEndian, info)
 }
