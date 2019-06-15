eb988a438ec5cc7f1240fcb5dfd8c6b46d0999e8
Lehner Florian <dev@der-flo.net>
2019-06-15 20:18:17 +0200

    Add Class
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- class.go
+++ class.go
@@ -2,6 +2,12 @@
 
 package tc
 
+import (
+	"fmt"
+
+	"github.com/mdlayher/netlink"
+)
+
 // Class represents the class part of rtnetlink
 type Class struct {
 	Tc
@@ -19,18 +25,39 @@ func (tc *Tc) Class() *Class {
 }
 
 // Add creats a new class
-func (c *Class) Add() error {
-	return ErrNotImplemented
+func (c *Class) Add(info *Object) error {
+	if info == nil {
+		return ErrNoArg
+	}
+	options, err := validateClassObject(rtmNewClass, info)
+	if err != nil {
+		return err
+	}
+	return c.action(rtmNewClass, netlink.Create|netlink.Excl, &info.Msg, options)
 }
 
 // Replace add/remove a class. If the node does not exist yet it is created
-func (c *Class) Replace() error {
-	return ErrNotImplemented
+func (c *Class) Replace(info *Object) error {
+	if info == nil {
+		return ErrNoArg
+	}
+	options, err := validateClassObject(rtmNewClass, info)
+	if err != nil {
+		return err
+	}
+	return c.action(rtmNewClass, netlink.Create, &info.Msg, options)
 }
 
 // Delete removes a class
-func (c *Class) Delete() error {
-	return ErrNotImplemented
+func (c *Class) Delete(info *Object) error {
+	if info == nil {
+		return ErrNoArg
+	}
+	options, err := validateClassObject(rtmDelClass, info)
+	if err != nil {
+		return err
+	}
+	return c.action(rtmDelClass, netlink.HeaderFlags(0), &info.Msg, options)
 }
 
 // Get fetches all classes
@@ -39,4 +66,12 @@ func (c *Class) Get(i *Msg) ([]Object, error) {
 		return []Object{}, ErrNoArg
 	}
 	return c.get(rtmGetClass, i)
+}
+
+func validateClassObject(action int, info *Object) ([]tcOption, error) {
+	options := []tcOption{}
+	if info.Ifindex == 0 {
+		return options, fmt.Errorf("Could not set device ID 0")
+	}
+	return options, nil
 }
