113f1928a97610d2ab8910f3a893e4c65115a75c
Lehner Florian <dev@der-flo.net>
2019-06-22 12:26:54 +0200

    Refactor Route4
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ f_route4.go
@@ -0,0 +1,75 @@
+package tc
+
+import (
+	"fmt"
+
+	"github.com/mdlayher/netlink"
+)
+
+const (
+	tcaRoute4Unspec = iota
+	tcaRoute4ClassID
+	tcaRoute4To
+	tcaRoute4From
+	tcaRoute4IIf
+	tcaRoute4Police
+	tcaRoute4Act
+)
+
+// Route4 contains attributes of the route discipline
+type Route4 struct {
+	ClassID uint32
+	To      uint32
+	From    uint32
+	IIf     uint32
+}
+
+//UnmarshalRoute4 parses the Route4-encoded data and stores the result in the value pointed to by info.
+func UnmarshalRoute4(data []byte, info *Route4) error {
+	ad, err := netlink.NewAttributeDecoder(data)
+	if err != nil {
+		return err
+	}
+	ad.ByteOrder = nativeEndian
+	for ad.Next() {
+		switch ad.Type() {
+		case tcaRoute4ClassID:
+			info.ClassID = ad.Uint32()
+		case tcaRoute4To:
+			info.To = ad.Uint32()
+		case tcaRoute4From:
+			info.From = ad.Uint32()
+		case tcaRoute4IIf:
+			info.IIf = ad.Uint32()
+		default:
+			return fmt.Errorf("UnmarshalRoute4()\t%d\n\t%v", ad.Type(), ad.Bytes())
+		}
+	}
+	return nil
+}
+
+// MarshalRoute4 returns the binary encoding of Route4
+func MarshalRoute4(info *Route4) ([]byte, error) {
+	options := []tcOption{}
+
+	if info == nil {
+		return []byte{}, fmt.Errorf("Route4 options are missing")
+	}
+
+	// TODO: improve logic and check combinations
+
+	if info.ClassID != 0 {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaRoute4ClassID, Data: info.ClassID})
+	}
+	if info.To != 0 {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaRoute4To, Data: info.To})
+	}
+	if info.From != 0 {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaRoute4From, Data: info.From})
+	}
+	if info.IIf != 0 {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaRoute4IIf, Data: info.IIf})
+	}
+
+	return marshalAttributes(options)
+}
