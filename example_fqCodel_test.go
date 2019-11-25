e506cb7db4d3c71f1fcbac5e7ba2b8a3bccc3f17
Lehner Florian <dev@der-flo.net>
2019-11-25 11:33:48 +0100

    Add fqCodel example
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ example_fqCodel_test.go
@@ -0,0 +1,76 @@
+// +build linux
+
+package tc_test
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/florianl/go-tc"
+	"golang.org/x/sys/unix"
+)
+
+func ExampleFqCodel() {
+	rtnl, err := tc.Open(&tc.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := rtnl.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "could not close rtnetlink socket: %v\n", err)
+		}
+	}()
+
+	devID, err := net.InterfaceByName("lo")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get interface ID: %v\n", err)
+		return
+	}
+
+	qdisc := tc.Object{
+		tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(devID.Index),
+			Handle:  tc.BuildHandle(0x1, 0x0),
+			Parent:  tc.HandleRoot,
+			Info:    0,
+		},
+		tc.Attribute{
+			Kind: "fq_codel",
+			// http://man7.org/linux/man-pages/man8/tc-fq_codel.8.html
+			// fq_codel limit 2000 target 3ms interval 40ms noecn
+			FqCodel: &tc.FqCodel{
+				Target:   0xbb8,
+				Limit:    0x7d0,
+				Interval: 0x9c40,
+				ECN:      0x0,
+			},
+		},
+	}
+
+	if err := rtnl.Qdisc().Add(&qdisc); err != nil {
+		fmt.Fprintf(os.Stderr, "could not assign htb to lo: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := rtnl.Qdisc().Delete(&qdisc); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete htb qdisc of lo: %v\n", err)
+			return
+		}
+	}()
+	qdiscs, err := rtnl.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get all qdiscs: %v\n", err)
+	}
+
+	for _, qdisc := range qdiscs {
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		fmt.Printf("%20s\t%s\n", iface.Name, qdisc.Kind)
+	}
+}
