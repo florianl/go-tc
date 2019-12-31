f7efc8399ecf4af3d9327eacdbd852c9334d685b
Lehner Florian <dev@der-flo.net>
2019-12-31 13:58:15 +0100

    update comment
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- internal/unix/types.go
+++ internal/unix/types.go
@@ -1,5 +1,5 @@
 /*
-Package internal/unix contains constants, that are needed to use github.com/florianl/go-tc.
+Package unix contains some constants, that are needed to use github.com/florianl/go-tc.
 Some of these constants are copied from golang.org/x/sys/unix to make them available also to
 other OS than linux. In the end, the only source of truth will be the Linux kernel itself.
 */
