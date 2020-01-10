a51b6ba7b5107e94964528acdfef31b00d279e43
Lehner Florian <dev@der-flo.net>
2020-01-10 23:19:08 +0100

    add netem example
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ example_netem_test.go
@@ -0,0 +1,76 @@
+// +build linux
+
+package tc_test
+
+import (
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/florianl/go-tc"
+	"golang.org/x/sys/unix"
+)
+
+func ExampleNetem() {
+	rtnl, err := tc.Open(&tc.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := rtnl.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "could not close rtnetlink socket: %v\n", err)
+		}
+	}()
+
+	devID, err := net.InterfaceByName("lo")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get interface ID: %v\n", err)
+		return
+	}
+
+	var ecn uint32 = 1
+	qdisc := tc.Object{
+		tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(devID.Index),
+			Handle:  tc.BuildHandle(0x1, 0x0),
+			Parent:  tc.HandleRoot,
+			Info:    0,
+		},
+		tc.Attribute{
+			Kind: "netem",
+			// tc qdisc replace dev tcDev root netem loss 1% ecn
+			Netem: &tc.Netem{
+				Qopt: tc.NetemQopt{
+					Limit: 1000,
+					Loss:  42949673},
+				Ecn: &ecn,
+			},
+		},
+	}
+
+	if err := rtnl.Qdisc().Replace(&qdisc); err != nil {
+		fmt.Fprintf(os.Stderr, "could not assign qdisc netem to lo: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := rtnl.Qdisc().Delete(&qdisc); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete netem qdisc of lo: %v\n", err)
+			return
+		}
+	}()
+	qdiscs, err := rtnl.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get all qdiscs: %v\n", err)
+	}
+
+	for _, qdisc := range qdiscs {
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		fmt.Printf("%20s\t%s\n", iface.Name, qdisc.Kind)
+	}
+}
