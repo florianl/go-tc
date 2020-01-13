cef9b9ad96a69a84b6d213e6d6b661c92feedda9
Lehner Florian <dev@der-flo.net>
2020-01-13 17:31:13 +0100

    add another integration test
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ q_pie_linux_integration_test.go
@@ -0,0 +1,117 @@
+//+build integration,linux
+
+package tc
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"testing"
+
+	"github.com/jsimonetti/rtnetlink"
+	"golang.org/x/sys/unix"
+)
+
+// setupDummyInterface installs a temporary dummy interface
+func setupDummyInterface(iface string) (*rtnetlink.Conn, error) {
+	con, err := rtnetlink.Dial(nil)
+	if err != nil {
+		return &rtnetlink.Conn{}, err
+	}
+
+	if err := con.Link.New(&rtnetlink.LinkMessage{
+		Family: unix.AF_UNSPEC,
+		Type:   unix.ARPHRD_NETROM,
+		Index:  0,
+		Flags:  unix.IFF_UP,
+		Change: unix.IFF_UP,
+		Attributes: &rtnetlink.LinkAttributes{
+			Name: iface,
+			Info: &rtnetlink.LinkInfo{Kind: "dummy"},
+		},
+	}); err != nil {
+		return con, err
+	}
+
+	return con, err
+}
+
+func TestLinuxTcQueuePie(t *testing.T) {
+	var rtnl *rtnetlink.Conn
+	var err error
+
+	tcIface := "tcIface"
+
+	if rtnl, err = setupDummyInterface(tcIface); err != nil {
+		fmt.Fprintf(os.Stderr, "could not setup dummy interface: %v\n", err)
+		return
+	}
+	defer rtnl.Close()
+
+	devID, err := net.InterfaceByName(tcIface)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get interface ID: %v\n", err)
+		return
+	}
+	defer func(devID uint32, rtnl *rtnetlink.Conn) {
+		if err := rtnl.Link.Delete(devID); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete interface: %v\n", err)
+		}
+	}(uint32(devID.Index), rtnl)
+
+	tcnl, err := Open(&Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := tcnl.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "could not close rtnetlink socket: %v\n", err)
+		}
+	}()
+
+	qdisc := Object{
+		Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(devID.Index),
+			Handle:  BuildHandle(0x1, 0x0),
+			Parent:  HandleRoot,
+			Info:    0,
+		},
+		//  tc qdisc add dev tcDev root pie limit 100 target 20ms tupdate 30ms ecn
+		// http://man7.org/linux/man-pages/man8/tc-pie.8.html
+		Attribute{
+			Kind: "pie",
+			Pie: &Pie{
+				Target:  0x4e20,
+				Limit:   0x64,
+				TUpdate: 0x7530,
+				ECN:     0x1,
+			},
+		},
+	}
+
+	if err := tcnl.Qdisc().Add(&qdisc); err != nil {
+		fmt.Fprintf(os.Stderr, "could not assign htb to lo: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := tcnl.Qdisc().Delete(&qdisc); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete htb qdisc of lo: %v\n", err)
+			return
+		}
+	}()
+	qdiscs, err := tcnl.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get all qdiscs: %v\n", err)
+	}
+
+	for _, qdisc := range qdiscs {
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		fmt.Printf("%20s\t%s\n", iface.Name, qdisc.Kind)
+	}
+}
