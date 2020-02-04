4c7eb31c5dd948d877c08c4894354f3af3599312
Lehner Florian <dev@der-flo.net>
2020-02-04 17:14:21 +0100

    add test
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ core/clock_test.go
@@ -0,0 +1,16 @@
+package core
+
+import "testing"
+
+func TestClock(t *testing.T) {
+	t.Run("Tick", func(t *testing.T) {
+		tick := Time2Tick(0xC0FFEE)
+		time := Tick2Time(tick)
+		// to avoid the dependency to math and make sure, diff is always equal or
+		// bigger than 0, diff is the square of the difference of both values
+		diff := (0xC0FFEE - time) * (0xC0FFEE - time)
+		if diff > 3 {
+			t.Fatalf("expected %d, got %d", 0xC0FFEE, time)
+		}
+	})
+}
