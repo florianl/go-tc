2e67ad5606e5feef00a08fe15e7bd74f644a0b2f
Lehner Florian <dev@der-flo.net>
2020-03-04 13:51:13 +0100

    add more tests
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- core/clock_test.go
+++ core/clock_test.go
@@ -1,6 +1,8 @@
 package core
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestClock(t *testing.T) {
 	t.Run("Tick", func(t *testing.T) {
@@ -20,6 +22,21 @@ func TestClock(t *testing.T) {
 		diff := (0xC0FFEE - time) * (0xC0FFEE - time)
 		if diff > 3 {
 			t.Fatalf("expected %d, got %d", 0xC0FFEE, time)
+		}
+	})
+
+	t.Run("XmitTime", func(t *testing.T) {
+		timeA := XmitTime(4096, 4096)
+		timeB := Time2Tick(timeUnitsPerSec)
+		if timeA != timeB {
+			t.Fatalf("expected %d, got %d", timeB, timeA)
+		}
+	})
+	t.Run("XmitSize", func(t *testing.T) {
+		a := XmitSize(timeUnitsPerSec, 1)
+		b := Tick2Time(1)
+		if a != b {
+			t.Fatalf("expected %d, got %d", a, b)
 		}
 	})
 }
