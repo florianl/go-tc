14b6a6f962a8fed1134c378390e2d5f1e2949899
Lehner Florian <dev@der-flo.net>
2020-04-18 10:48:43 +0200

    make linter happy with comments
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- internal/unix/types_linux.go
+++ internal/unix/types_linux.go
@@ -4,8 +4,10 @@ package unix
 
 import linux "golang.org/x/sys/unix"
 
+// IfInfomsg makes unix.IfInfomsg available
 type IfInfomsg = linux.IfInfomsg
 
+// various constants
 const (
 	AF_UNSPEC     = linux.AF_UNSPEC
 	NETLINK_ROUTE = linux.NETLINK_ROUTE
@@ -14,24 +16,28 @@ const (
 	RTNLGRP_TC    = linux.RTNLGRP_TC
 )
 
+// various constants
 const (
 	RTM_NEWTFILTER = linux.RTM_NEWTFILTER
 	RTM_DELTFILTER = linux.RTM_DELTFILTER
 	RTM_GETTFILTER = linux.RTM_GETTFILTER
 )
 
+// various constants
 const (
 	RTM_NEWTCLASS = linux.RTM_NEWTCLASS
 	RTM_DELTCLASS = linux.RTM_DELTCLASS
 	RTM_GETTCLASS = linux.RTM_GETTCLASS
 )
 
+// various constants
 const (
 	RTM_NEWQDISC = linux.RTM_NEWQDISC
 	RTM_DELQDISC = linux.RTM_DELQDISC
 	RTM_GETQDISC = linux.RTM_GETQDISC
 )
 
+// various constants
 const (
 	RTM_NEWCHAIN = linux.RTM_NEWCHAIN
 	RTM_DELCHAIN = linux.RTM_DELCHAIN
