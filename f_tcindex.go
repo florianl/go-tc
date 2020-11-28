ab74c054c1de50170be892bd5fc35a94f578e648
Florian Lehner <dev@der-flo.net>
2020-11-28 14:04:05 +0100

    add tcindex
    Signed-off-by: Florian Lehner <dev@der-flo.net>

---
+++ f_tcindex.go
@@ -0,0 +1,80 @@
+package tc
+
+import (
+	"fmt"
+
+	"github.com/mdlayher/netlink"
+)
+
+const (
+	tcaTcIndexUnspec = iota
+	tcaTcIndexHash
+	tcaTcIndexMask
+	tcaTcIndexShift
+	tcaTcIndexFallThrough
+	tcaTcIndexClassID
+	tcaTcIndexPolice
+	tcaTcIndexAct
+)
+
+type TcIndex struct {
+	Hash        *uint32
+	Mask        *uint16
+	Shift       *uint32
+	FallThrough *uint32
+	ClassID     *uint32
+}
+
+// marshalTcIndex returns the binary encoding of TcIndex
+func marshalTcIndex(info *TcIndex) ([]byte, error) {
+	options := []tcOption{}
+
+	if info == nil {
+		return []byte{}, fmt.Errorf("TcIndex: %w", ErrNoArg)
+	}
+
+	// TODO: improve logic and check combinations
+	if info.Hash != nil {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaTcIndexHash, Data: uint32Value(info.Hash)})
+	}
+	if info.Mask != nil {
+		options = append(options, tcOption{Interpretation: vtUint16, Type: tcaTcIndexMask, Data: uint16Value(info.Mask)})
+	}
+	if info.Shift != nil {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaTcIndexShift, Data: uint32Value(info.Shift)})
+	}
+	if info.FallThrough != nil {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaTcIndexFallThrough, Data: uint32Value(info.FallThrough)})
+	}
+	if info.ClassID != nil {
+		options = append(options, tcOption{Interpretation: vtUint32, Type: tcaTcIndexClassID, Data: uint32Value(info.ClassID)})
+	}
+
+	return marshalAttributes(options)
+}
+
+// unmarshalTcIndex parses the TcIndex-encoded data and stores the result in the value pointed to by info.
+func unmarshalTcIndex(data []byte, info *TcIndex) error {
+	ad, err := netlink.NewAttributeDecoder(data)
+	if err != nil {
+		return err
+	}
+	ad.ByteOrder = nativeEndian
+	for ad.Next() {
+		switch ad.Type() {
+		case tcaTcIndexHash:
+			info.Hash = uint32Ptr(ad.Uint32())
+		case tcaTcIndexMask:
+			info.Mask = uint16Ptr(ad.Uint16())
+		case tcaTcIndexShift:
+			info.Shift = uint32Ptr(ad.Uint32())
+		case tcaTcIndexFallThrough:
+			info.FallThrough = uint32Ptr(ad.Uint32())
+		case tcaTcIndexClassID:
+			info.ClassID = uint32Ptr(ad.Uint32())
+		default:
+			return fmt.Errorf("unmarshalTcIndex()\t%d\n\t%v", ad.Type(), ad.Bytes())
+		}
+	}
+	return nil
+}
