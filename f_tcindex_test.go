6a19599012de500e65c05ef55bbb8eed76f32eaa
Florian Lehner <dev@der-flo.net>
2020-12-12 11:49:04 +0100

    add tcindex test
    Signed-off-by: Florian Lehner <dev@der-flo.net>

---
+++ f_tcindex_test.go
@@ -0,0 +1,50 @@
+package tc
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestTcIndex(t *testing.T) {
+	tests := map[string]struct {
+		val  TcIndex
+		err1 error
+		err2 error
+	}{
+		"empty": {},
+		"simple": {val: TcIndex{Hash: uint32Ptr(1), Mask: uint16Ptr(2), Shift: uint32Ptr(3),
+			FallThrough: uint32Ptr(4), ClassID: uint32Ptr(5)}},
+	}
+
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			data, err1 := marshalTcIndex(&testcase.val)
+			if err1 != nil {
+				if testcase.err1 != nil && errors.Is(err1, testcase.err1) {
+					return
+				}
+				t.Fatalf("Unexpected error: %v", err1)
+			}
+			val := TcIndex{}
+			err2 := unmarshalTcIndex(data, &val)
+			if err2 != nil {
+				if testcase.err2 != nil && errors.Is(err2, testcase.err2) {
+					return
+				}
+				t.Fatalf("Unexpected error: %v", err2)
+
+			}
+			if diff := cmp.Diff(val, testcase.val); diff != "" {
+				t.Fatalf("TcIndex missmatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+	t.Run("nil", func(t *testing.T) {
+		_, err := marshalTcIndex(nil)
+		if !errors.Is(err, ErrNoArg) {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+}
