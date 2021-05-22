dadd4f1229aa08112311eed46dcdb40999f9dc2e
Florian Lehner <dev@der-flo.net>
2021-05-22 09:47:54 +0200

    extend test
    Signed-off-by: Florian Lehner <dev@der-flo.net>

---
+++ stats_test.go
@@ -0,0 +1,49 @@
+package tc
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestGenStats(t *testing.T) {
+	tests := map[string]struct {
+		val  GenStats
+		err1 error
+		err2 error
+	}{
+		"simple": {val: GenStats{Basic: &GenBasic{Bytes: 123}}},
+	}
+
+	for name, testcase := range tests {
+		t.Run(name, func(t *testing.T) {
+			data, err1 := marshalGenStats(&testcase.val)
+			if err1 != nil {
+				if testcase.err1 != nil && errors.Is(err1, testcase.err1) {
+					return
+				}
+				t.Fatalf("Unexpected error: %v", err1)
+			}
+			val := GenStats{}
+			newData := injectAttribute(t, data, []byte{}, tcaStatsPad)
+			err2 := unmarshalGenStats(newData, &val)
+			if err2 != nil {
+				if testcase.err2 != nil && errors.Is(err2, testcase.err2) {
+					return
+				}
+				t.Fatalf("Unexpected error: %v", err2)
+
+			}
+			if diff := cmp.Diff(val, testcase.val); diff != "" {
+				t.Fatalf("GenStats missmatch (want +got):\n%s", diff)
+			}
+		})
+	}
+	t.Run("nil", func(t *testing.T) {
+		_, err := marshalGenStats(nil)
+		if !errors.Is(err, ErrNoArg) {
+			t.Fatalf("unexpected error: %v", err)
+		}
+	})
+}
