63da4e96c613f8ed040c77076bd9891aa94b1b6a
Florian Lehner <dev@der-flo.net>
2021-10-23 10:15:01 +0200

    move test helpers to test file
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- ratetable.go
+++ ratetable.go
@@ -4,11 +4,9 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"testing"
 
 	"github.com/florianl/go-tc/core"
 	"github.com/florianl/go-tc/internal/unix"
-	"github.com/mdlayher/netlink"
 )
 
 // iproute2/tc/tc_core.c:tc_calc_rtable()
@@ -80,37 +78,4 @@ func adjustSize(sz, mpu, linklayer uint) uint32 {
 	default:
 		return uint32(sz)
 	}
-}
-
-func skipAttribute(t *testing.T, typ uint16, skip []uint16) bool {
-	t.Helper()
-
-	for _, s := range skip {
-		if s == typ {
-			return true
-		}
-	}
-	return false
-}
-
-// stripRateTable is a helper function used only in tests.
-func stripRateTable(t *testing.T, orig []byte, skip []uint16) ([]byte, error) {
-	t.Helper()
-
-	var attrs []netlink.Attribute
-
-	ad, err := netlink.NewAttributeDecoder(orig)
-	if err != nil {
-		return []byte{}, err
-	}
-	for ad.Next() {
-		if !skipAttribute(t, ad.Type(), skip) {
-			attrs = append(attrs, netlink.Attribute{
-				Type: ad.Type(),
-				Data: ad.Bytes(),
-			})
-		}
-	}
-
-	return netlink.MarshalAttributes(attrs)
 }
--- ratetable_test.go
+++ ratetable_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/florianl/go-tc/internal/unix"
+	"github.com/mdlayher/netlink"
 )
 
 var (
@@ -229,4 +230,37 @@ func TestAdjustSize(t *testing.T) {
 			}
 		})
 	}
+}
+
+func skipAttribute(t *testing.T, typ uint16, skip []uint16) bool {
+	t.Helper()
+
+	for _, s := range skip {
+		if s == typ {
+			return true
+		}
+	}
+	return false
+}
+
+// stripRateTable is a helper function used only in tests.
+func stripRateTable(t *testing.T, orig []byte, skip []uint16) ([]byte, error) {
+	t.Helper()
+
+	var attrs []netlink.Attribute
+
+	ad, err := netlink.NewAttributeDecoder(orig)
+	if err != nil {
+		return []byte{}, err
+	}
+	for ad.Next() {
+		if !skipAttribute(t, ad.Type(), skip) {
+			attrs = append(attrs, netlink.Attribute{
+				Type: ad.Type(),
+				Data: ad.Bytes(),
+			})
+		}
+	}
+
+	return netlink.MarshalAttributes(attrs)
 }
