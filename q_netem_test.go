14c2b0efac3e8f77e00215f71c7e138bde32829d
Florian Lehner <dev@der-flo.net>
2022-07-15 08:32:19 +0200

    add DelayDist
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- q_netem_test.go
+++ q_netem_test.go
@@ -5,15 +5,23 @@ import (
 	"io"
 	"testing"
 
+	"github.com/florianl/go-tc/core"
 	"github.com/google/go-cmp/cmp"
 )
 
 func TestNetem(t *testing.T) {
+	delayDist := []int16{9, 7, 5, 3, 1}
 	tests := map[string]struct {
 		val  Netem
 		err1 error
 		err2 error
 	}{
+		"delayDist": {val: Netem{
+			Qopt: NetemQopt{
+				Latency: core.Time2Tick(2),
+				Jitter:  core.Time2Tick(3),
+			},
+			DelayDist: &delayDist}},
 		"simple": {val: Netem{Ecn: uint32Ptr(123), Latency64: int64Ptr(-4242), Jitter64: int64Ptr(-1337)}},
 		"qopt":   {val: Netem{Qopt: NetemQopt{Latency: 42}, Rate64: uint64Ptr(1337)}},
 		"random": {val: Netem{Corr: &NetemCorr{Delay: 2}, Reorder: &NetemReorder{Correlation: 13}, Corrupt: &NetemCorrupt{Correlation: 11}, Rate: &NetemRate{PacketOverhead: 1337}, Slot: &NetemSlot{MinDelay: 2, MaxDelay: 4}}},
