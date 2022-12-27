9aa0222f3764f37b845c4a149d278434462facc3
Florian Lehner <dev@der-flo.net>
2022-12-27 10:04:27 +0100

    improve error handling in tests
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- m_connmark_test.go
+++ m_connmark_test.go
@@ -20,7 +20,7 @@ func TestConnmark(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			data, err1 := marshalConnmark(&testcase.val)
 			if err1 != nil {
-				if testcase.err1 != nil && testcase.err1.Error() == err1.Error() {
+				if errors.Is(err1, testcase.err1) {
 					return
 				}
 				t.Fatalf("Unexpected error: %v", err1)
@@ -30,7 +30,7 @@ func TestConnmark(t *testing.T) {
 			val := Connmark{}
 			err2 := unmarshalConnmark(newData, &val)
 			if err2 != nil {
-				if testcase.err2 != nil && testcase.err2.Error() == err2.Error() {
+				if errors.Is(err2, testcase.err2) {
 					return
 				}
 				t.Fatalf("Unexpected error: %v", err2)
