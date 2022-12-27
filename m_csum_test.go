4e93a450a9dcc5e35ab285b1521f08ebd7b92922
Florian Lehner <dev@der-flo.net>
2022-12-27 10:04:27 +0100

    improve error handling in tests
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- m_csum_test.go
+++ m_csum_test.go
@@ -21,7 +21,7 @@ func TestCsum(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			data, err1 := marshalCsum(&testcase.val)
 			if err1 != nil {
-				if testcase.err1 != nil && testcase.err1.Error() == err1.Error() {
+				if errors.Is(err1, testcase.err1) {
 					return
 				}
 				t.Fatalf("Unexpected error: %v", err1)
@@ -31,7 +31,7 @@ func TestCsum(t *testing.T) {
 			val := Csum{}
 			err2 := unmarshalCsum(newData, &val)
 			if err2 != nil {
-				if testcase.err2 != nil && testcase.err2.Error() == err2.Error() {
+				if errors.Is(err2, testcase.err2) {
 					return
 				}
 				t.Fatalf("Unexpected error: %v", err2)
