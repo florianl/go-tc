2ba0657dda3210c030a287456307c82e0cc113b5
Florian Lehner <dev@der-flo.net>
2023-03-16 12:17:22 +0100

    Improve documentation in eBPF example
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- example_gteq_1.16_test.go
+++ example_gteq_1.16_test.go
@@ -20,7 +20,7 @@ import (
 func Example_eBPF() {
 	tcIface := "ExampleEBPF"
 
-	// Set up a dummy interface for the example.
+	// For the purpose of testing a dummy network interface is set up for the example.
 	rtnl, err := setupDummyInterface(tcIface)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not setup dummy interface: %v\n", err)
@@ -34,11 +34,14 @@ func Example_eBPF() {
 		return
 	}
 	defer func(devID uint32, rtnl *rtnetlink.Conn) {
+		// As a dummy network interface was set up for this test make sure to
+		// remove this link again once this example finished.
 		if err := rtnl.Link.Delete(devID); err != nil {
 			fmt.Fprintf(os.Stderr, "could not delete interface: %v\n", err)
 		}
 	}(uint32(devID.Index), rtnl)
 
+	// Open a netlink/tc connection to the Linux kernel.
 	tcnl, err := tc.Open(&tc.Config{})
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
@@ -50,6 +53,8 @@ func Example_eBPF() {
 		}
 	}()
 
+	// Create a qdisc/clsact object that will be attached to the ingress part
+	// of the networking interface.
 	qdisc := tc.Object{
 		Msg: tc.Msg{
 			Family:  unix.AF_UNSPEC,
@@ -63,19 +68,25 @@ func Example_eBPF() {
 		},
 	}
 
+	// Attach the qdisc/clsact to the networking interface.
 	if err := tcnl.Qdisc().Add(&qdisc); err != nil {
 		fmt.Fprintf(os.Stderr, "could not assign clsact to %s: %v\n", tcIface, err)
 		return
 	}
-	// when deleting the qdisc, the applied filter will also be gone
+	// When deleting the qdisc, the applied filter will also be gone
 	defer tcnl.Qdisc().Delete(&qdisc)
 
-	// Handcraft a eBPF program for the example.
+	// Handcraft an eBPF program of type BPF_PROG_TYPE_SCHED_CLS that will be attached to
+	// the networking interface via qdisc/clsact.
+	//
+	// For eBPF programs of type BPF_PROG_TYPE_SCHED_CLS the returned code defines the action that
+	// will be applied to the network packet. Returning 0 translates to TC_ACT_OK and will terminate
+	// the packet processing pipeline within netlink/tc and allows the packet to proceed.
 	spec := ebpf.ProgramSpec{
 		Name: "test",
 		Type: ebpf.SchedCLS,
 		Instructions: asm.Instructions{
-			// set exit code to 0
+			// Set exit code to 0
 			asm.Mov.Imm(asm.R0, 0),
 			asm.Return(),
 		},
@@ -92,6 +103,7 @@ func Example_eBPF() {
 	fd := uint32(prog.FD())
 	flags := uint32(0x1)
 
+	// Create a tc/filter object that will attach the eBPF program to the qdisc/clsact.
 	filter := tc.Object{
 		tc.Msg{
 			Family:  unix.AF_UNSPEC,
@@ -108,6 +120,8 @@ func Example_eBPF() {
 			},
 		},
 	}
+
+	// Attach the tc/filter object with the eBPF program to the qdisc/clsact.
 	if err := tcnl.Filter().Add(&filter); err != nil {
 		fmt.Fprintf(os.Stderr, "could not attach filter for eBPF program: %v\n", err)
 		return
