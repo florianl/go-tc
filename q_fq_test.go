21f9f363908c553f50090097d01731a0301f5d97
Brad Cowie <brad@faucet.nz>
2024-04-17 10:02:17 +1200

    Add support for new FQ qdisc options.
      * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=583396f4ca4d6
      * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=39d010504e6b4
      * https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=49e7265fd098f

--- q_fq_test.go
+++ q_fq_test.go
@@ -8,12 +8,51 @@ import (
 )
 
 func TestFq(t *testing.T) {
+	weights := []int32{589824, 196608, 65536}
 	tests := map[string]struct {
 		val  Fq
 		err1 error
 		err2 error
 	}{
-		"simple": {val: Fq{PLimit: uint32Ptr(1), FlowPLimit: uint32Ptr(2), Quantum: uint32Ptr(3), InitQuantum: uint32Ptr(4), RateEnable: uint32Ptr(5), FlowDefaultRate: uint32Ptr(6), FlowMaxRate: uint32Ptr(7), BucketsLog: uint32Ptr(8), FlowRefillDelay: uint32Ptr(9), OrphanMask: uint32Ptr(10), LowRateThreshold: uint32Ptr(11), CEThreshold: uint32Ptr(12)}},
+		"simple": {
+			val: Fq{
+				PLimit:           uint32Ptr(1),
+				FlowPLimit:       uint32Ptr(2),
+				Quantum:          uint32Ptr(3),
+				InitQuantum:      uint32Ptr(4),
+				RateEnable:       uint32Ptr(5),
+				FlowDefaultRate:  uint32Ptr(6),
+				FlowMaxRate:      uint32Ptr(7),
+				BucketsLog:       uint32Ptr(8),
+				FlowRefillDelay:  uint32Ptr(9),
+				OrphanMask:       uint32Ptr(10),
+				LowRateThreshold: uint32Ptr(11),
+				CEThreshold:      uint32Ptr(12),
+			},
+		},
+		"defaults": {
+			val: Fq{
+				PLimit:           uint32Ptr(10000),
+				FlowPLimit:       uint32Ptr(100),
+				Quantum:          uint32Ptr(3028),
+				InitQuantum:      uint32Ptr(15140),
+				RateEnable:       uint32Ptr(1),
+				FlowMaxRate:      uint32Ptr(4294967295),
+				BucketsLog:       uint32Ptr(10),
+				FlowRefillDelay:  uint32Ptr(40000),
+				OrphanMask:       uint32Ptr(1023),
+				LowRateThreshold: uint32Ptr(68750),
+				CEThreshold:      uint32Ptr(4294967295),
+				TimerSlack:       uint32Ptr(10000),
+				Horizon:          uint32Ptr(10000000),
+				HorizonDrop:      uint8Ptr(1),
+				PrioMap: &FqPrioQopt{
+					Bands:   3,
+					PrioMap: [16]uint8{1, 2, 2, 2, 1, 2, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+				},
+				Weights: &weights,
+			},
+		},
 	}
 
 	for name, testcase := range tests {
