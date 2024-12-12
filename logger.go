2ee7ddf04370956428c59ffac0335c4353189b67
Quang Nguyen <nguyenquang@microsoft.com>
2024-12-12 18:34:32 -0500

    Update comment formatting in logger.go

--- logger.go
+++ logger.go
@@ -1,6 +1,6 @@
 package tc
 
-// The logger interface only requires a Printf method for now as that is the only method used in the package.
+// Logger interface only requires a Printf method for now as that is the only method used in the package.
 type Logger interface {
 	Printf(format string, v ...interface{})
 }
