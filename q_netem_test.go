f981838ee0049bc82893a9a14a413b9f8cbea26f
Florian Lehner <dev@der-flo.net>
2024-12-30 16:08:39 +0100

    fix formating
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- q_netem_test.go
+++ q_netem_test.go
@@ -25,7 +25,7 @@ func TestNetem(t *testing.T) {
 		"simple":   {val: Netem{Ecn: uint32Ptr(123), Latency64: int64Ptr(-4242), Jitter64: int64Ptr(-1337)}},
 		"qopt":     {val: Netem{Qopt: NetemQopt{Latency: 42}, Rate64: uint64Ptr(1337)}},
 		"random":   {val: Netem{Corr: &NetemCorr{Delay: 2}, Reorder: &NetemReorder{Correlation: 13}, Corrupt: &NetemCorrupt{Correlation: 11}, Rate: &NetemRate{PacketOverhead: 1337}, Slot: &NetemSlot{MinDelay: 2, MaxDelay: 4}}},
-		"prngseed":     {val: Netem{Qopt: NetemQopt{Latency: 42}, Rate64: uint64Ptr(1337), PrngSeed: uint64Ptr(31337)}},
+		"prngseed": {val: Netem{Qopt: NetemQopt{Latency: 42}, Rate64: uint64Ptr(1337), PrngSeed: uint64Ptr(31337)}},
 	}
 
 	for name, testcase := range tests {
